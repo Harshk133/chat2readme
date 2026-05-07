05f63836286fa8c6c16f5f86b0a8b8150a7f4654
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2026-05-07 15:29:26 +0530

    .

--- app/website/backend/index.py
+++ app/website/backend/index.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 CORS(
     app,
     resources={
-        r"/*": {
+        r"/convert": {
             "origins": [
                 "https://chat2readme-o3fv.vercel.app"
             ]
